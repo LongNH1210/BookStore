546b440af2bef76f03d0450898fc1e80b6b4ed3f
LongNH1210 <130813132+LongNH1210@users.noreply.github.com>
2024-01-08 18:38:33 +0700

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Models;
+
+namespace BookStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Category { get; set; } = default!;
+        public DbSet<Book> Book { get; set; } = default!;
+        public DbSet<CartDetail> CartDetail { get; set; } = default!;
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<CartDetail> CartDetails { get; set; }
+        public DbSet<Order> Order { get; set; }
+
+
+    }
+}
